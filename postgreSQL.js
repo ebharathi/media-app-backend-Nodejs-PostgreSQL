5a0563324b1214e9bf052e0604b7a90530831a7f
ebharathi <elayabharathi10102002@gmail.com>
2023-10-20 17:07:49 +0530

    added login

--- postgreSQL.js
+++ postgreSQL.js
@@ -1,6 +1,6 @@
-const {Client}=require('pg');
-
-const client=new Client(
+const {Client,Pool}=require('pg');
+const bcrypt=require('bcrypt')
+const pool=new Pool(
     {
         host:'127.0.0.1',
         user:'postgres',
@@ -9,11 +9,11 @@ const client=new Client(
         port:5432
     }
 )
-const insert=async(name,password)=>{
+const signup=async(name,password)=>{
+  const client=await pool.connect()
+  console.log("[+]CONNECTED")
   try {
      console.log("[+]INSERTION CALLED[+]")
-     await client.connect();
-     console.log("[+]CONNECTED")
      const result=await client.query('INSERT INTO users(name,password)VALUES($1,$2) RETURNING id',[name,password]);
      console.log("[+]INSERTION DONE");
     //  console.log("-->",result);
@@ -31,8 +31,58 @@ const insert=async(name,password)=>{
     }
   }
   finally{
-    await client.end();
+    await client.release();
     console.log("[+]DISCONNECTED")
   }
 }
-module.exports={insert}
+//login
+const login=async(reqName,reqPassword)=>{
+  const client=await pool.connect();
+  console.log("[+]CONNECTED");
+  try {
+     console.log("[+]RETRIEVE OPERATION CALLED");
+    //  await client.connect();
+     const result=await client.query('SELECT * FROM users WHERE name=$1',[reqName]);
+    //  console.log("response--->",result);
+     const user=result.rows[0];
+     if(user)
+     {
+         const isPasswordSame=await bcrypt.compare(reqPassword,user.password)
+               if(isPasswordSame)
+               {
+                  // console.log("[+]user exists")
+                  return {
+                    error:false,
+                    message:"User login successful"
+                  }
+                }
+                else
+                {
+                  console.log("PASSWORD DOEN'T MATCH");
+                  return {
+                    error:true,
+                    message:"User doesn't exist"
+                  }
+               }
+     }
+     else{
+        console.log("[-]USER DOES NOT EXISTS")
+        return {
+          error:true,
+          message:"User doesn't exist."
+        }
+     }
+  } catch (error) {
+      console.log("[-]LOGIN QUERY FAILED");
+      console.log("--->",error);
+      return {
+        error:true,
+        message:error.detail
+      }
+  }
+  finally{
+    await client.release();
+    console.log("[+]DISCONNECTED");
+  }
+}
+module.exports={signup,login}
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router=require('express').Router();
-const {insert}=require('../postgreSQL')
+const {signup,login}=require('../postgreSQL')
 const bcrypt=require('bcrypt')
 const jwt=require('jsonwebtoken')
 router.post('/signup',async(req,res)=>{
@@ -7,7 +7,7 @@ router.post('/signup',async(req,res)=>{
          console.log("[+]SIGNUP CALLED..........");
          const {username,password}=req.body;
          const hashedPassword=await bcrypt.hash(password,10);
-         await insert(username,hashedPassword).then((response)=>{
+         await signup(username,hashedPassword).then((response)=>{
             if(response.error==false)
               res.json({
                 error:false,
@@ -27,6 +27,36 @@ router.post('/signup',async(req,res)=>{
             error:true,
             message:error.message
          })
+    }
+})
+router.post('/login',async(req,res)=>{
+    try {
+        console.log("[+]LOGIN CALLED....");
+        const {username,password}=req.body;
+        await login(username,password).then((response)=>{
+            if(response.error==false)
+            {
+                console.log("[+]CHECKING CONFIRMED")
+                res.json({
+                    error:false,
+                    message:response.message
+                })
+            }
+            else{
+                // console.log("[+]CHECKING FAILED")
+                res.json({
+                    error:true,
+                    message:response.message
+                })
+            }
+        })
+    } catch (error) {
+        console.error("[-]ERROR IN ROUTER API FOR LOGIN");
+        console.log("---?",error)
+        res.json({
+           error:true,
+           message:error.message
+        })
     }
 })
 module.exports={router}
